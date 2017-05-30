a97475570d94988a1b728aa0e90476b8279e45ba
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-05-30 18:11:52 -0500

    fix pinView

--- javascripts/controllers/PinViewCtrl.js
+++ javascripts/controllers/PinViewCtrl.js
@@ -1,13 +1,11 @@
 app.controller("PinViewCtrl", function($scope, $routeParams , PinFactory ) {
    $scope.pins = [];
 
-   let getPins = () => {
    PinFactory.getPinList($routeParams.id).then((results) => {
-		$scope.pins = results.data;
+		$scope.pins = results;
 		console.log("results", results);
 	}).catch((error) => {
 		console.log("getPinList", error);
-	})
-};
-	getPins();
+	});
+
 });
