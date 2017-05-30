5be4a000d67b247a92a5c2273cde58efea0eeb13
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-05-29 23:11:28 -0500

    boardCtrl

--- javascripts/controllers/BoardListCtrl.js
+++ javascripts/controllers/BoardListCtrl.js
@@ -1,3 +1,17 @@
-app.controller("BoardListCtrl", function() {
+app.controller("BoardListCtrl", function($scope, ItemFactory) {
+
+	$scope.items = [];
+
+	let getItems = () => {
+		ItemFactory.getItemList().then((itemz) => {
+			console.log ("cntrl" , itemz);
+			// $scope.items = itemz;
+		}).catch((error) => {
+			console.log("getItem error" , error)
+		});
+	};
+
+
+	getItems();
 
 });
