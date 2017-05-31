b0e8293b0932733deb77713c60069127bb042ff7
Ben Greaves <bsgreaves@gmail.com>
2017-05-30 19:26:57 -0500

    BoardFactory.js complete

--- javascripts/controllers/BoardListCtrl.js
+++ javascripts/controllers/BoardListCtrl.js
@@ -2,15 +2,15 @@ app.controller("BoardListCtrl", function($scope, BoardFactory) {
 
 	$scope.boards = [];
 
-	let getBoards = () => {
-		BoardFactory.getBoardList().then((boardz) => {
+	let getMyBoards = () => {
+		BoardFactory.FBgetSingleUserBoards().then((boardz) => {
 			console.log ("cntrl" , boardz);
 			$scope.boards = boardz;
 		}).catch((error) => {
-			console.log("getBoard error" , error)
+			console.log("getBoard error" , error);
 		});
 	};
 
-	getBoards();
+	getMyBoards();
 
 });
