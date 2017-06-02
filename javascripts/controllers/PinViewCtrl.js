0f32e61087613955bf4de43ec18b7447d30db7bf
Ben Greaves <bsgreaves@gmail.com>
2017-06-01 23:19:07 -0500

    Good progress. Board list partial good, working on pin view partial. About to remap the routes.

--- javascripts/controllers/PinViewCtrl.js
+++ javascripts/controllers/PinViewCtrl.js
@@ -1,9 +1,20 @@
-app.controller("PinViewCtrl", function($scope, $routeParams , PinFactory ) {
-   $scope.pins = [];
+app.controller("PinViewCtrl", function($rootScope, $scope, $routeParams, BoardFactory, PinFactory, UserFactory) {
 
-   PinFactory.getPinList($routeParams.id).then((results) => {
+	$scope.ownerUsername = "";
+	$scope.pins = [];
+  $scope.board = [];
+
+	BoardFactory.FBgetSingleBoard($routeParams.id)
+	.then(board => {
+		$scope.board = board;
+		return UserFactory.getUser($scope.board.uid);
+	}, error => console.log("error in FBgetSingleBoard in PinViewCtrl"))
+	.then(user => $scope.ownerUsername = user.username)
+	.catch(error => console.log("Error in getUser in PinViewCtrl", error));
+	$scope.isOwner = $scope.board.uid === $rootScope.user.uid ? true : false;
+	
+  PinFactory.getPinList($routeParams.id).then((results) => {
 		$scope.pins = results;
-		console.log("results", results);
 	}).catch((error) => {
 		console.log("getPinList", error);
 	});
