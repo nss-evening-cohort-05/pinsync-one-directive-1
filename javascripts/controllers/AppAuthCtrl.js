731e1897c43981948770f77b039a29d2a55c686e
Mitchell Blom <mitchellblom@gmail.com>
2017-06-02 13:51:06 -0500

    message change in error, edited usernames

--- javascripts/controllers/AppAuthCtrl.js
+++ javascripts/controllers/AppAuthCtrl.js
@@ -16,7 +16,7 @@ app.controller("AppAuthCtrl", function($location, $rootScope, $scope, AuthFactor
 		AuthFactory.authenticate($scope.auth).then((userCreds) => {
 			return UserFactory.getUser(userCreds.uid);
 		}, (error) => {
-			$scope.alerts.push({msg: error.message});
+			$scope.alerts.push({msg: "Oops! Looks like that username or password isn't recognized."});
 			console.log(error);
 		}).then((user) => {
 			$rootScope.user = user;
