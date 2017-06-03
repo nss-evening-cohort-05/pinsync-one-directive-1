33bb8f34a5b22ce669ccf2de046f488daef7bf30
drworm75 <dwgames@gmail.com>
2017-06-03 11:41:33 -0500

    commit for pin edit pull

--- javascripts/controllers/PinEditCtrl.js
+++ javascripts/controllers/PinEditCtrl.js
@@ -1,3 +1,13 @@
 app.controller("PinEditCtrl", function() {
 
+	$scope.newTask = {};
+
+  ItemFactory.getSingleItem($routeParams.id).then((results) => {
+    $scope.newTask = results.data;
+  	console.log("get single item results", results);
+    results.data.dueDate = new Date(results.data.dueDate);
+  }).catch((error) => {
+  	console.log("getSingleItem", error);
+  });
+  
 });
